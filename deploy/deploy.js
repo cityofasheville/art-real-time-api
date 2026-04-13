5a721463a1d85b965bf73b8dbd5facafe355085f
alletizia <aletizia529@gmail.com>
2026-04-13 11:17:51 -0400

    Update terraform config

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,5 +1,7 @@
 // This script sets up the "build" directory structure for deployment to AWS Lambda
 import fs from 'fs/promises';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { execSync } from 'child_process';
 import YAML from 'yaml';
 
@@ -7,13 +9,14 @@ import { vpc_settings } from './settings/vpc_settings.js';
 import { build_main } from './build_main.js';
 import { build_role } from './build_role.js';
 
-try {
-  const __deploy_dir = `${process.cwd()}/deploy`; // current directory
 
-  const file = await fs.readFile(`${process.cwd()}/deploy.yaml`, 'utf8');
+try {
+  const __deploy_dir = dirname(fileURLToPath(import.meta.url)); // current directory
+
+  const file = await fs.readFile(`${__deploy_dir}/deploy.yaml`, 'utf8');
   let config = YAML.parse(file);
   config.deploy_dir = __deploy_dir;
-  const gitBranch = execSync('git branch --show-current').toString().trim();
+  const gitBranch = execSync('git branch --show-current').toString().trim().split('/').pop();
 
   // get Lambda environment variables from .env file
   try {
@@ -33,13 +36,13 @@ try {
   // determine Lambda name
   if (deploy_type === 'production' || deploy_type === 'main') {
     config.prog_name = config.lambda_name;
-    config.build_dir = `${__deploy_dir}/build/prod`;
+    config.build_dir = 'build/prod';
   } else if (deploy_type === 'development') {
     config.prog_name = config.lambda_name + '_dev';
-    config.build_dir = `${__deploy_dir}/build/dev`;
+    config.build_dir = 'build/dev';
   } else {
     config.prog_name = config.lambda_name + '_' + deploy_type;
-    config.build_dir = `${__deploy_dir}/build/${deploy_type}`;
+    config.build_dir = 'build/' + deploy_type;
   }
 
   // Set domain name and certificate for API Gateway
@@ -74,17 +77,16 @@ try {
 
   // Set up files for deployment
   setUpFiles(config);
-} catch (err) {
+}
+catch (err) {
   console.log(err);
 }
 
-
 async function setUpFiles(config) {
-  const deploy_dir = config.deploy_dir;
-  const build_dir = config.build_dir;
+  let build_dir = config.build_dir;
   // create empty build dir
-  await fs.rm(build_dir, { force: true, recursive: true });
-  await fs.mkdir(build_dir, { force: true, recursive: true });
+  await fs.rm('./' + build_dir, { force: true, recursive: true });
+  await fs.mkdir('./' + build_dir, { force: true, recursive: true });
 
   // build config.tf file
   await build_main(config);
@@ -94,24 +96,24 @@ async function setUpFiles(config) {
 
   // Copy files into "funcdir" subdirectory for zip files
   await fs.mkdir(`${build_dir}/funcdir`);
-  await fs.cp('./src/', `${build_dir}/funcdir/`, { recursive: true });
-  await fs.copyFile('./package.json', `${build_dir}/funcdir/package.json`);
+  await fs.cp('../src/', `${build_dir}/funcdir/`, { recursive: true });
+  await fs.copyFile('../package.json', `${build_dir}/funcdir/package.json`);
 
   // Build NodeJS or Python
   if (config.nodejs_or_python === 'nodejs') {
     await BuildNodeJS(build_dir);
   } else if (config.nodejs_or_python === 'python') {
-    await fs.copyFile(`${deploy_dir}/template.yaml`, `${build_dir}/template.yaml`)
+    await fs.copyFile('template.yaml', `${build_dir}/template.yaml`)
     await BuildPython(build_dir, config.sam_deploy);
   }
 ///////////////////////////////////
- execSync(`cd ${build_dir} && terraform init && terraform apply -auto-approve`, { stdio: 'inherit' });
+ execSync(`cd ${build_dir} && terraform init && terraform apply`, { stdio: 'inherit' });
 ///////////////////////////////////
 }
 
 async function BuildPython(build_dir, sam_deploy) {
   // FOR PYTHON: Copy files into "python" and "funcdir" subdirectories for zip files
-  await fs.copyFile('./src/requirements.txt', `${build_dir}/requirements.txt`);
+  await fs.copyFile('../src/requirements.txt', `${build_dir}/requirements.txt`);
   await fs.mkdir(`${build_dir}/python`);
   await fs.mkdir(`${build_dir}/python/python`);
   if (sam_deploy === 'true') {
@@ -125,8 +127,8 @@ async function BuildPython(build_dir, sam_deploy) {
 async function BuildNodeJS(build_dir) {
   // FOR NODEJS: Copy files into "nodejs" and "funcdir" subdirectories for zip files
   await fs.mkdir(`${build_dir}/nodejs`);
-  await fs.copyFile('./package.json', `${build_dir}/nodejs/package.json`);
-  await fs.copyFile('./package-lock.json', `${build_dir}/nodejs/package-lock.json`);
+  await fs.copyFile('../package.json', `${build_dir}/nodejs/package.json`);
+  await fs.copyFile('../package-lock.json', `${build_dir}/nodejs/package-lock.json`);
 
-  execSync(`npm install --prefix ${build_dir}/nodejs --omit-dev`, { stdio: 'inherit' });
+  execSync(`npm install --prefix ${build_dir}/nodejs --omit=dev`, { stdio: 'inherit' });
 }
